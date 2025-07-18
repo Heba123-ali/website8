3f01ece8ab2831daa4a298170c1a42b4bd10259d
Heba Taha Ali <132607564+Heba123-ali@users.noreply.github.com>
2025-07-18 21:14:23 +0300

    finish

--- main.js
+++ main.js
@@ -45,3 +45,21 @@ mobileIcon.addEventListener("click", ()=>{
         }
       }
     });
+  
+const backToTopBtn = document.querySelector(".back-to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+});
+
+backToTopBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
